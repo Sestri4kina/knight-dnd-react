cae2e2fc8f6bf30be5d452c756d7dd7fbe1fba4c
Anastasiia Bratochkina <bratochkina@seniordevonly.com>
2017-06-14 14:23:20 +0300

    Add drag and drop logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import PropTypes from 'prop-types';
 import Knight from './Knight';
-import Square from './Square';
+import BoardSquare from './BoardSquare';
 import { canMoveKnight, moveKnight } from './Game';
 
 class Board extends Component {
@@ -16,20 +16,23 @@ class Board extends Component {
     renderSquare(i) {
         const x = i % 8;
         const y = Math.floor(i / 8);
-        const black = (x + y) % 2 === 1;
-
-        const [knightX, knightY] = this.props.knightPosition;
-        const piece = (x === knightX && y === knightY) ? <Knight /> : null;
-
+        
         return (
-        <div key={i}
-            style={{ width: '12.5%', height: '12.5%' }}
-            onClick={() => this.handleSquareClick(x, y)}>
-            <Square black={black}>
-                { piece }
-            </Square>
-        </div>
+            <div key={i}
+                style={{ width: '12.5%', height: '12.5%' }}
+                >
+                <BoardSquare x={x} y={y}>
+                    { this.renderPiece(x, y) }
+                </BoardSquare>
+            </div>
         );
+    }
+
+    renderPiece(x, y) {
+        const [knightX, knightY] = this.props.knightPosition;
+        if (x === knightX && y === knightY) {
+            return <Knight />;
+        }
     }
 
     handleSquareClick(toX, toY) {
